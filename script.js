89e9444ca80673fdb2c5fddd26f7ab122affcbc7
masseyblack <106741762+masseyblack@users.noreply.github.com>
2026-01-03 07:27:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/* Dark / Light Toggle */
+const toggleBtn = document.getElementById("themeToggle");
+const html = document.documentElement;
+
+toggleBtn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+  toggleBtn.textContent = html.classList.contains("dark")
+    ? "☀️ Light"
+    : "🌙 Dark";
+});
+
+/* Countdown Timer */
+const launchDate = new Date("2026-02-01T00:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = launchDate - now;
+
+  document.getElementById("days").textContent =
+    Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  document.getElementById("hours").textContent =
+    Math.floor((diff / (1000 * 60 * 60)) % 24);
+
+  document.getElementById("minutes").textContent =
+    Math.floor((diff / (1000 * 60)) % 60);
+
+  document.getElementById("seconds").textContent =
+    Math.floor((diff / 1000) % 60);
+}, 1000);
+const form = document.getElementById("notifyForm");
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // stop page reload
+
+  successMessage.classList.remove("hidden");
+
+  // Optional: reset form
+  form.reset();
+});
+
